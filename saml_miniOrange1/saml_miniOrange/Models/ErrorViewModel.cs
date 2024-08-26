de315aef1f262ae711cf43de27435da4f4258649
Nagesh Ajab <nageshajab@hotmail.com>
2024-08-26 14:38:30 +0530

    initial commit

---
+++ saml_miniOrange1/saml_miniOrange/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace saml_miniOrange.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ saml_miniOrange1/saml_miniOrange/Program.cs
@@ -0,0 +1,45 @@
+using miniOrange.saml;
+using System.Reflection;
+
+namespace saml_miniOrange
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddminiOrangeServices(Assembly.GetExecutingAssembly());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseminiOrangeSAMLSSOMiddleware();   
+
+            app.Run();
+        }
+    }
+}
